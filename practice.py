f93955e3455c5451d462a228b094114c5d1ff478
Park-Judong <84074206+Park-Judong@users.noreply.github.com>
2025-10-16 23:25:27 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -531,8 +531,8 @@
 #     gun = gun - soldiers
 #     print("[함수 내] 남은 총 : {0}".format(gun))
 
-# # RECOMMENDED!!!!!!!!!!!!!!!
-# # we can deal with the global gun as a parameter of this func. better for managing your code
+# RECOMMENDED!!!!!!!!!!!!!!!
+# we can deal with the global gun as a parameter of this func. better for managing your code
 # def checkpoint_ret(gun, soldiers):
 #     gun = gun - soldiers
 #     print("[함수 내] 남은 총 : {0}".format(gun))
@@ -541,3 +541,117 @@
 # print("전체 총 : {0}".format(gun))
 # gun = checkpoint_ret(gun,2) # 2명이 경계 근무 나감
 # print("남은 총 : {0}".format(gun))
+
+# height = float(input("키가 몇입니까?(m) : "))
+# gender = input("성별이 무엇입니까?(ex. 남성, 여성) : ")
+
+# def std_weight(height, gender):
+#     if gender == '남성':
+#         std_kg = height * height * 22
+#     elif gender == '여성':
+#         std_kg = height * height * 21
+#     return std_kg
+
+# std_kg = std_weight(height, gender)
+
+# print(f"키 {str(int(100*height))}cm {gender}의 표준 치중은 {std_kg:.2f}kg 입니다.")
+
+#standard input/output
+# print("Python", "Java", "JavaScript", sep=",", end="?")
+# print("무엇이 더 재밌을까요?")
+
+# import sys
+# print("Python", "Java", file=sys.stdout)
+# print("Python", "Java", file=sys.stderr)
+
+# scores = {"수학":0, "영어":50, "코딩":100}
+# for subject, score in scores.items(): #key: subject, value: score
+#     print(subject.ljust(8), str(score).rjust(4), sep=':')
+
+#은행 대기순번표
+#001, 002, 003, ...
+# for num in range(1,21):
+#     print("대기번호 : " + str(num).zfill(3))
+
+# answer = input("아무값이나 입력하세요: ")
+# print(type(answer))
+
+# various output format
+# # 빈자리는 빈공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
+# print("{0: >10}".format(500))
+# # 양수일땐 +로 표시, 음수일땐 -로 표시
+# print("{0: >+10}".format(500))
+# print("{0: >+10}".format(-500))
+# # 왼쪽 정렬하고, 빈칸으로 _로 채움
+# print("{0:_<+10}".format(500))
+# # 3자리마다 콤마를 찍어주기
+# print("{0:,}".format(100000000000000))
+# # 3자리마다 콤마를 찍어주기, +- 부호도 붙이기
+# print("{0:+,}".format(100000000000000))
+# print("{0:+,}".format(-100000000000000))
+# # 3자리마다 콤마를 찍어주기, +- 부호도 붙이기, 자리수 확보하기
+# print("{0:^<+30,}".format(100000000000000))
+# # 소수점 출력 2번째자리까지
+# print("{0:.2f}".format(5/3))
+
+#file input/output
+# score_file = open("score.txt", "w", encoding="utf8") #utf8: 한글정보
+# print("수학: 0", file=score_file)
+# print("영어: 50", file=score_file)
+# score_file.close()
+
+# score_file = open("score.txt", "w", encoding="utf8") #utf8: 한글정보
+# score_file.write("과학 : 80")
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8") #utf8: 한글정보
+# #print(score_file.read()) #read all lines at once
+# #print(score_file.readline()) #read line by line
+# #print(score_file.readline()) #read line by line
+# while True:
+#     line = score_file.readline()
+#     if not line:
+#         break
+#     print(line, end="")
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8") #utf8: 한글정보
+# lines = score_file.readlines() #list 형태로 저장
+# for line in lines:
+#     print(line,end="")
+# score_file.close()
+
+#pickle
+# import pickle
+# profile_file = open("profile.pickle", "wb") #write
+# profile = {"이름":"박명수", "나이":30, "취미":["축구", "골프", "코딩"]}
+# print(profile)
+# pickle.dump(profile, profile_file) #profile에 있는 정보를 file에 저장
+# profile_file.close()
+
+# import pickle
+
+# profile_file = open("profile.pickle", "rb") #read
+# profile = pickle.load(profile_file) #file에 있는 정보를 profile에 불러오기
+# print(profile)
+# profile_file.close()
+
+#with
+# import pickle
+# with open("profile.pickle", "rb") as profile_file:
+#     print(pickle.load(profile_file))
+
+# with open("study.txt", "w", encoding="utf8") as study_file:
+#     study_file.write("파이썬을 열심히 공부하고 있어요")
+
+# with open("study.txt", "r", encoding="utf8") as study_file:
+#     print(study_file.read())
+
+# for i in range(1,3): #1~2
+#     with open(f"{i}주차.txt", "w", encoding="utf8") as report_file:
+#         dep = input("부서명: ")
+#         name = input("이름: ")
+#         work = input("업무 요약: ")
+#         report_file.write(f"부서 : {dep}\n")
+#         report_file.write(f"이름 : {name}\n")
+#         report_file.write(f"업무 요약 : {work}\n")
